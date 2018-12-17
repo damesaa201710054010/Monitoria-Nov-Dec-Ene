aeff092db73018c9aedac8dc4f0c9005b9ac4765
camilopaez <kamilopaez19@gmail.com>
2018-12-17 14:59:31 -0500

    talleres

---
+++ Talleres/Datos2/taller10/codigo_estudiante/java/Taller10.java
@@ -0,0 +1,21 @@
+/**
+ * Clase en la cual se implementan los metodos del Taller 10
+ * 
+ * @author Mauricio Toro, Andres Paez
+ */
+public class Taller10{
+
+	/**
+	* Metodo que calcula la longitud de la subsecuencia mas larga en comun entre dos cadenas
+	* 
+	* @param x cadena de caracteres
+	* @param y cadena de caracteres
+	* algunas opciones
+	* @see <a href="https://es.wikipedia.org/wiki/Algoritmo_Knuth-Morris-Pratt">KMP</a>
+	* @see <a href="https://es.wikipedia.org/wiki/Algoritmo_de_b%C3%BAsqueda_de_cadenas_Boyer-Moore">Boyer Moore y sus variantes</a>
+	*
+	*/
+    public static int lcs(String x, String y) {
+
+    }
+}
---
+++ Talleres/Datos2/taller11/codigo_estudiante/Taller11.java
@@ -0,0 +1,23 @@
+
+/**
+ * Clase en la cual se implementan los metodos del Taller 11
+ * 
+ * @author Mauricio Toro, Andres Paez
+ */
+public class Taller11 {
+
+	/**
+	* Dado un grafo completo, hallen el costo mínimo del recorrido que pasa por
+	* todos los vértices exactamente una vez y vuelve al nodo inicial utilizando
+	* programación dinámica
+	* @param g grafo dado
+	* @param y cadena de caracteres
+	* para mas informacion
+	* @see <a href="https://en.wikipedia.org/wiki/Held%E2%80%93Karp_algorithm">KMP</a>
+	*
+	*/
+	public static int heldKarp(Digraph g) {
+	
+	}
+
+}
