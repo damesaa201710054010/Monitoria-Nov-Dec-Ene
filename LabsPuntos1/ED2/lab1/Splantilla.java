5524fa8ce6c70101bee3e7fd0dd7fcc4cd3e217b
Daniel Mesa <damesaa@eafit.edu.co>
2019-01-21 20:15:28 -0500

    plantilla lab 1 datos 2

---
+++ LabsPuntos1/ED2/lab1/Splantilla.java
@@ -0,0 +1,26 @@
+/**
+* Esta clase debe de contener la solucion al problema planteado en el punto 1
+* del laboratorio#1 del curso de estructura de datos y algoritmos II
+* @author Daniel Mesa, Mauricio Toro
+* @version 1.0
+*/
+public class splantilla
+{
+  /**
+  * El metodo debe de leer el archivo y construir la estrcutura de datos con el mapa
+  *
+  */
+  public static void makeMap()
+  {
+
+  }
+
+  /**
+  * Metodo principal del programa
+  * @param args un array de argumentos
+  */
+  public static void main(String[] args)
+  {
+
+  }
+}
