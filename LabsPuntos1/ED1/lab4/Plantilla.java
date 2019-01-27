1c03dcf07ba2673fdac943bc704d8affe637c026
Daniel Mesa <damesaa@eafit.edu.co>
2019-01-27 16:42:23 -0500

    plantilla lab4

---
+++ LabsPuntos1/ED1/lab4/Plantilla.java
@@ -0,0 +1,60 @@
+/**
+ * Esta clase contiene los metodos que dan solucion al problema planteado en el laboratorio numero 4 del curso de estructura  * de datos y algoritmos II Nota: solo contiene las consultas que debe de hacer, deben de implementar los metodos
+ * y operaciones que consideren necesario ademas pueden hacer las modificaciones que necesiten 
+ * @author Daniel Mesa, Mauricio Toro
+ * @version 1.0
+ */
+public class Plantilla
+{
+    /**
+     * Este metodo se encarga de leer el archivo con los datos de entrada
+     * utilizando bufferedreader
+     * @param fileName, un string con el nombre del archivo a leer
+     */
+    public static void reading(String fileName)
+    {
+
+    }
+
+    /**
+     * Este metodo realiza una consulta a la estructura de datos de manera eficiente y los imprime
+     * @param directoryName, string con el nombre del directorio o subdiretorio consultado
+     */
+    public static void [] consult(String directoryName)
+    {
+
+    }
+
+    /**
+     * Este metodo imprime la consulta a un directorio o subdirectorio filtrando por el dueño de este
+     * @param directoryName, nombre del directorio o subdirectorio en el cual realizar la consulta
+     * @param user, nombre del usuario por el cual filtrar los directorios o subdirectorios para realizar la consulta
+     */
+    public static void consultForUser(String directoryName, String user)
+    {
+
+    }
+
+    /**
+     * Este metodo imprime la consulta a un directorio o subdirectorio filtrando por el tamaño
+     * @param directoryName, nombre del directorio o subdirectorio en el cual realizar la consulta
+     * @param size, tamaño por el cual se deben de filtrar los resultados
+     */
+    public static String consultForSize(String directoryName, int size)
+    {
+	
+    }
+
+
+    /**
+     * Metodo principal del programa
+     * @param args, un array de argumentos
+     */
+    public static void main(String[] args)
+    {
+	
+    }
+    
+
+    
+}
